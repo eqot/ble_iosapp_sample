2cb80b164ffc4736ca54acca9f1148d97f1496cf
Ikuo Terado <eqobar@gmail.com>
2015-05-04 00:28:09 +0900

    Minor changes

--- features/BLE.js
+++ features/BLE.js
@@ -4,7 +4,6 @@ var BLENative = require('NativeModules').BLENative;
 
 class BLE {
   startScanning(): Promise {
-    console.log('0');
     return new Promise((resolve, reject) => {
       BLENative.startScanning(function() {
         resolve();
@@ -13,7 +12,6 @@ class BLE {
   }
 
   scanPeripherals(): Promise {
-    console.log('1');
     return new Promise((resolve, reject) => {
       BLENative.scanPeripherals(function(name, id) {
         resolve(name);
